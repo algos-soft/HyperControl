2ea7fdc19db8e39831bec4183ff54b3ae8020897
algos-soft <info@algos.it>
2015-07-24 19:25:31 +0200

    .

--- app/src/main/java/it/technocontrolsystem/hypercontrol/display/MenuDisplay.java
+++ app/src/main/java/it/technocontrolsystem/hypercontrol/display/MenuDisplay.java
@@ -1,11 +1,15 @@
 package it.technocontrolsystem.hypercontrol.display;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
 import it.technocontrolsystem.hypercontrol.HyperControlApp;
+import it.technocontrolsystem.hypercontrol.Lib;
 import it.technocontrolsystem.hypercontrol.activity.MenuActivity;
+import it.technocontrolsystem.hypercontrol.communication.Connection;
 import it.technocontrolsystem.hypercontrol.communication.MenuCommandRequest;
 import it.technocontrolsystem.hypercontrol.database.DB;
 import it.technocontrolsystem.hypercontrol.domain.Menu;
@@ -18,7 +22,7 @@ public class MenuDisplay extends Button {
 
     MenuActivity activity;
 
-    public MenuDisplay(MenuActivity activity,int menuId) {
+    public MenuDisplay(final MenuActivity activity,int menuId) {
         super(activity);
 
         this.activity=activity;
@@ -35,7 +39,6 @@ public class MenuDisplay extends Button {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent();
-
                     intent.putExtra("siteid", act.getSite().getId());
                     intent.putExtra("title", getText());
                     intent.putExtra("pageid", menu.getNumber());
@@ -47,8 +50,33 @@ public class MenuDisplay extends Button {
             setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    MenuCommandRequest request = new MenuCommandRequest(menu.getNumber());
-                    HyperControlApp.sendRequest(request);
+
+                    String errMsg=null;
+                    if(Lib.isNetworkAvailable()) {
+                        Connection conn=HyperControlApp.getConnection();
+                        if((conn!=null) && conn.isOpen()){
+                            MenuCommandRequest request = new MenuCommandRequest(menu.getNumber());
+                            HyperControlApp.sendRequest(request);
+                        }else{
+                            errMsg="Connessione chiusa";
+                        }
+                    }else{
+                        errMsg="Network non disponibile";
+                    }
+
+                    if(errMsg!=null){
+                        AlertDialog.Builder builder = new  AlertDialog.Builder(activity);
+                        builder.setMessage("Impossibile inviare il comando\n"+errMsg);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        builder.show();
+                    }
+
+
                 }
             });
 
